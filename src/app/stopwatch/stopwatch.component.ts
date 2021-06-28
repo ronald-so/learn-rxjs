694b80e13daa1cb1292c9aed1d574f20a12fbc6b
rwhs <ronald.so1996@gmail.com>
2021-06-27 22:37:05 -0400

    Linter + Prettier, rxjs

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-stopwatch',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stopwatch.component.scss']
 })
 export class StopwatchComponent implements OnInit {
-
   constructor() {
+    console.log(this.builtInTenthSecondInterval$);
   }
 
   ngOnInit(): void {
+    //
   }
+
+  manualTenthSecondInterval$ = new Observable(observer => {
+    let counter = 0;
+    observer.next(counter);
+
+    const interval = setInterval(() => {
+      counter++;
+      observer.next(counter);
+    }, 100);
+    return function unsubscribe() { clearInterval(interval) };
+  })
+
+  builtInTenthSecondInterval$ = interval(100);
 
 }
