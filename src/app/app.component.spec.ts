4260848089a20c56040a892d4af8772bea49244a
rwhs <ronald.so1996@gmail.com>
2021-06-30 02:55:20 -0400

    Updated stopwatch

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'learn-rxjs'`, () => {
+  it('should have as title \'learn-rxjs\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('learn-rxjs');
--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stopwatch',
@@ -7,25 +8,45 @@ import { interval, Observable } from 'rxjs';
   styleUrls: ['./stopwatch.component.scss']
 })
 export class StopwatchComponent implements OnInit {
+  timer: number;
+  manualTenthSecondInterval$: Observable<any>;
+  builtInTenthSecondInterval$: Observable<any>;
+
   constructor() {
-    console.log(this.builtInTenthSecondInterval$);
+    this.timer = 0;
+    this.manualTenthSecondInterval$ = new Observable(observer => {
+      let counter = 0;
+      observer.next(counter);
+
+      const interval = setInterval(() => {
+        counter++;
+        observer.next(counter);
+      }, 100);
+      return function unsubscribe() { clearInterval(interval) };
+    })
+    this.builtInTenthSecondInterval$ = interval(100);
   }
 
   ngOnInit(): void {
-    //
+    console.log('Initialized stopwatch component');
   }
 
-  manualTenthSecondInterval$ = new Observable(observer => {
-    let counter = 0;
-    observer.next(counter);
+  displayAsTenthOfSeconds(): void {
+    this.builtInTenthSecondInterval$
+      .pipe(
+        map(tenthSecond => tenthSecond / 10),
+      )
+      .subscribe(res => {
+        this.timer = res;
+      })
+  }
 
-    const interval = setInterval(() => {
-      counter++;
-      observer.next(counter);
-    }, 100);
-    return function unsubscribe() { clearInterval(interval) };
-  })
+  startButtonClicked(): void {
+    console.log('Start button clicked');
+    this.displayAsTenthOfSeconds();
+  }
 
-  builtInTenthSecondInterval$ = interval(100);
-
+  stopButtonClicked(): void {
+    console.log('Stop button clicked')
+  }
 }
